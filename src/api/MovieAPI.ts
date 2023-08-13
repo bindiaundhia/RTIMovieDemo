8e55b4345d7707b0646b5d45b7b390b2d5d13b09
bindiaundhia <>
2023-08-13 21:25:40 +0530

    Removed API token

--- src/api/MovieAPI.ts
+++ src/api/MovieAPI.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3/discover/movie",
   headers: {
-    Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZDQ1YmNhYmQyMDhlZDU5OWNlN2Y0NjRjMTAyNzAzZiIsInN1YiI6IjY0ZDdjOTRiMzcxMDk3MDBmZmI1YjE3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A4BOOE4kDo4FFnwXckqwFFE8MpYkpCzlaD9bLgiHFuY"}`,
+    Authorization: `Bearer ${"Put your API key here"}`,
   },
 });
 
